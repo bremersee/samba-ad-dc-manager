d3a742cd5b4d5d8c9ddc9d07e385edede4189811
Christian Bremer <bremersee@googlemail.com>
2026-01-25 22:36:20 +0100

    Init.

--- src/main/java/org/bremersee/samba/ad/dc/service/EmailChangeAesCryptoService.java
+++ src/main/java/org/bremersee/samba/ad/dc/service/EmailChangeAesCryptoService.java
@@ -1,15 +1,7 @@
 package org.bremersee.samba.ad.dc.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.bremersee.exception.ServiceException;
 import org.bremersee.samba.ad.dc.config.ApplicationProperties;
-import org.bremersee.samba.ad.dc.model.AesEncValue;
 import org.bremersee.samba.ad.dc.model.EmailChange;
-import org.bremersee.samba.ad.dc.model.PasswordReset;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-import org.springframework.security.crypto.encrypt.Encryptors;
-import org.springframework.security.crypto.keygen.KeyGenerators;
 import org.springframework.stereotype.Service;
 
 @Service
