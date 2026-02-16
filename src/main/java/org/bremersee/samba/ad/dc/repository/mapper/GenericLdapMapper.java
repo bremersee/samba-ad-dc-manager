456fcab2208c9626db92948bfa8eb29856d741ff
Christian Bremer <bremersee@googlemail.com>
2026-02-17 00:58:38 +0100

    Init.

--- src/main/java/org/bremersee/samba/ad/dc/repository/mapper/GenericLdapMapper.java
+++ src/main/java/org/bremersee/samba/ad/dc/repository/mapper/GenericLdapMapper.java
@@ -16,6 +16,8 @@
 
 package org.bremersee.samba.ad.dc.repository.mapper;
 
+import static java.util.Objects.requireNonNullElseGet;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -36,12 +38,12 @@ public class GenericLdapMapper {
   private final String[] binaryAttributeNames;
 
   public GenericLdapMapper(List<AdEntryLdapMapperDelegate<?>> delegates) {
-    this.delegates = delegates;
+    this.delegates = requireNonNullElseGet(delegates, List::of);
     this.mappedAttributeNames = this.delegates.stream()
         .flatMap(delegate -> Arrays.stream(delegate.getBinaryAttributeNames()))
         .distinct()
         .toArray(String[]::new);
-    this.binaryAttributeNames = delegates.stream()
+    this.binaryAttributeNames = this.delegates.stream()
         .flatMap(delegate -> Arrays.stream(delegate.getBinaryAttributeNames()))
         .distinct()
         .toArray(String[]::new);
